b03652fa7f3f72f72104434a41195827c40db037
Joe-86 <jiujiu1229@gmail.com>
2016-10-18 23:45:32 -0700

    Update lab2 Jianqiao Zhou.asm

--- lab2 Jianqiao Zhou.asm
+++ lab2 Jianqiao Zhou.asm
@@ -1,3 +1,21 @@
+/*
+III. Write a program called checkEven.asm. The program checks if an 8-bit unsigned number is an
+even number. Here is the algorithm:
+ R18 <- 0x09
+ Make all other bits, except the lest significant bit of R18 to 0 (hint: use AND)
+ Compare R18 with 0x00
+ Set R19 to 0x00
+ If (R18 == 0x00)
+R19 <- 0x01
+ Done
+*/
+
+
+
+
+
+
+
 .cseg	;select current segmant as code
 .org 0	;begin assembling at address 0
 
